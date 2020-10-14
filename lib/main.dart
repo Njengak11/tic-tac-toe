c0ba45beb38581c73d07e0d16645aa81b4600bef
njenga1011 <njenga.k11@gmail.com>
2020-10-14 11:13:26 +0300

    added button

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tic_tac_toe/homepage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 String title = "Tic Tac Toe";
 void main() {
@@ -13,9 +14,62 @@ class MyApp extends StatelessWidget {
       title: title,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: Colors.amber
+        primaryColor: Colors.grey[800]
       ),
-      home: HomePage(),
+      home: IntroScreen(),
+    );
+  }
+}
+
+class IntroScreen extends StatefulWidget {
+  @override
+  _IntroScreenState createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       backgroundColor: Colors.grey[800],
+      appBar: AppBar(
+        
+        elevation: 0.0,
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text('TIC-TAC-TOE',
+           style:GoogleFonts.pressStart2p(
+         color: Colors.white,
+         fontSize: 30
+       )
+          ),
+          Center(
+            child: ButtonTheme(
+              height: 80,
+              minWidth: 200,
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+                color: Colors.white,
+                onPressed: (){
+                  Navigator.push(context, 
+                  MaterialPageRoute(builder: (context) => HomePage()
+                  ));
+                },
+                child: Text('START',
+                 style:GoogleFonts.pressStart2p(
+         color: Colors.black,
+         fontSize: 20
+       )
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
