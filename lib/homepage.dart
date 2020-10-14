ea8d021d6f26d44a8537020156b1746cd95b63de
njenga1011 <njenga.k11@gmail.com>
2020-10-14 11:45:58 +0300

    added gameplay

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 
@@ -9,6 +10,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+bool exTurn = true;
 List<String> displayExOh = [
   '','','','','','','','','',
 ];
@@ -50,10 +52,89 @@ List<String> displayExOh = [
 
   void _tapped(int index){
    setState((){
-   displayExOh[index] = 'X';
+   if(exTurn){
+     displayExOh[index ]= 'X';
+   }else{
+     displayExOh[index ]= 'O';
+   }
+
+   exTurn =!exTurn;
+   _checkWinner();
       });
    }
+
+   void _checkWinner(){
+     //row 1
+     if( displayExOh[0] == displayExOh[1] && 
+         displayExOh[0] == displayExOh[2] && 
+         displayExOh[0] !=''){
+       _showWinDialog(displayExOh[0]);
+     }
+
+       //row 2
+     if( displayExOh[3] == displayExOh[4] && 
+         displayExOh[3] == displayExOh[5] && 
+         displayExOh[3] !=''){
+       _showWinDialog(displayExOh[3]);
+     }
+
+       //row 3
+     if( displayExOh[6] == displayExOh[7] && 
+         displayExOh[6] == displayExOh[8] && 
+         displayExOh[6] !=''){
+       _showWinDialog(displayExOh[6]);
+     }
+
+       //column 1
+     if( displayExOh[0] == displayExOh[3] && 
+         displayExOh[0] == displayExOh[6] && 
+         displayExOh[0] !=''){
+       _showWinDialog(displayExOh[0]);
+     }
+
+       //column 2
+     if( displayExOh[1] == displayExOh[4] && 
+         displayExOh[1] == displayExOh[7] && 
+         displayExOh[1] !=''){
+       _showWinDialog(displayExOh[1]);
+     }
+
+       //column 3
+     if( displayExOh[2] == displayExOh[5] && 
+         displayExOh[2] == displayExOh[8] && 
+         displayExOh[2] !=''){
+       _showWinDialog(displayExOh[2]);
+     }
+
+       //diagonal
+     if( displayExOh[6] == displayExOh[4] && 
+         displayExOh[6] == displayExOh[2] && 
+         displayExOh[6] !=''){
+       _showWinDialog(displayExOh[6]);
+     }
+
+       //diagonal
+     if( displayExOh[0] == displayExOh[4] && 
+         displayExOh[0] == displayExOh[8] && 
+         displayExOh[0] !=''){
+       _showWinDialog(displayExOh[0]);
+     }
 }
+
+void _showWinDialog( String winner){
+  showDialog(context: context,
+  builder: (BuildContext context){
+    return AlertDialog(
+      title: Text('WINNER IS: '+ winner,
+       style:GoogleFonts.pressStart2p(
+         color: Colors.black,
+         fontSize: 20)
+      ),
+    );
+  });
+}
+}
+
 
 
    
